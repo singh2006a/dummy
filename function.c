9474d805ed671fffb21161c48291ce39b9251aa5
singh2006a <singhanshika31239@gmail.com>
2025-09-15 19:42:58 +0530

    Add files via upload

---
+++ function.c
@@ -0,0 +1,31 @@
+// #include<stdio.h>
+// int sum(int a){
+//     sum();
+// }
+// int sum()
+// {
+//    int a=10;
+//    int b=12;
+//    int c;
+//    c=a+b;
+//    printf("%d",c);
+//    return 0;
+// printf("%d",a);
+// }
+// int main(){
+    // sum(14);
+    // return(0);
+    // printf("hello");
+
+// }
+#include<stdio.h>
+int sum(){
+    int a=43;
+    int b=10;
+    int c=a+b;
+    return c;
+}
+int main(){
+    int data=sum();
+    printf("%d",data);
+}

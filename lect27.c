a99815fb11653a38f5d3d8e153a8a8c775bb1a55
Anshika Singh <singhanshika31239@gmail.com>
2025-10-30 00:56:48 +0530

    max profit

---
+++ lect27.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    // int arr[6]={7,1,5,3,6,4};
+    // int minbuy=arr[0];
+    // int maxP=0;
+    // for(int i=1;i<6;i++){
+    //     if(arr[i]<minbuy){
+    //         minbuy=arr[i];
+    //     }else{
+    //         int profit=arr[i]-minbuy;
+    //         if(profit>maxP){
+    //             maxP=profit;
+    //         }
+    //     }
+    //     printf("%d\n",maxP);
+    // }
+
+
+    // int n=7;
+    // if(n&1){
+    //     printf("odd");
+    // }else{
+    //     printf("even");
+    // }
+}
